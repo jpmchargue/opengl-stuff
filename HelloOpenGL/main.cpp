77560a85f8fa3f286cd04acf4cea8a462bce5df2
James McHargue <jpm221@case.edu>
2022-05-03 00:18:11 -0400

    Initial Commit

---
+++ HelloOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloOpenGL
+//
+//  Created by James McHargue on 5/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
